03053797fffae8ca2aec1574abff2a00a922e194
Artem Dadashjants <111521698+ketronix-dev@users.noreply.github.com>
2024-07-13 21:20:44 +0300

    Update Context.cs

--- Contexts/Context.cs
+++ Contexts/Context.cs
@@ -19,7 +19,7 @@ public class Context : IdentityDbContext<AuthUser>
             $"Database=api;" +
             $"Username=postgres;" +
             $"Password=password;" +
-            $"Port=5432;");
+            $"Port=5433;");
         
     }
     
